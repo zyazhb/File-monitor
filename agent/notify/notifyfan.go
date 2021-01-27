0b34375418bdf71cafa0f2d47c7e3c55da32844b
ZYA <zyazhb@yahoo.com>
2021-01-27 10:05:02 +0800

    Fix fanotify rpc

--- agent/notify/notifyfan.go
+++ agent/notify/notifyfan.go
@@ -30,55 +30,64 @@ func RunFanotify(mountpoint string, hashflag bool, serverip string) {
 		logger.Fatal("\033[1;31m", err, "\033[0m")
 	}
 
-	f := func(notify *fanotify.NotifyFD) (string, error) {
-		data, err := notify.GetEvent(os.Getpid())
-		if err != nil {
-			return "", fmt.Errorf("%w", err)
-		}
-
-		if data == nil {
-			return "", nil
-		}
-
-		defer data.Close()
-
-		path, err := data.GetPath()
-		if err != nil {
-			return "", err
-		}
-
-		switch {
-		case data.MatchMask(unix.FAN_ACCESS):
-			return fmt.Sprintf("FAN_ACCESS file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_OPEN):
-			return fmt.Sprintf("Open file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_ATTRIB):
-			return fmt.Sprintf("FAN_ATTRIB file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_CREATE):
-			return fmt.Sprintf("FAN_CREATE file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_DELETE):
-			return fmt.Sprintf("FAN_DELETE file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_MODIFY):
-			return fmt.Sprintf("FAN_MODIFY file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_CLOSE):
-			return fmt.Sprintf("FAN_CLOSE file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_MOVE):
-			return fmt.Sprintf("FAN_MOVE file: PID:%d %s", data.GetPID(), path), nil
-		case data.MatchMask(unix.FAN_CLOSE_WRITE):
-			return fmt.Sprintf("FAN_CLOSE_WRITE file: PID:%d %s", data.GetPID(), path), nil
-		}
-
-		return "", fmt.Errorf("fanotify: unknown event")
-	}
-
 	for {
-		str, err := f(notify)
-		if err == nil && len(str) > 0 {
-			logger.Info("\033[1;33m [*]", str, "\033[0m")
+		event, pid, path, err := RealFanotify(notify)
+		var filehash string
+		if hashflag {
+			filehash = calcHash(path) //计算hash
+		}
+		if serverip != "" {
+			go rpcreportfan(path, filehash, event, serverip) //rpc上报
+		}
+		if err == nil && pid != -1 {
+			logger.Info("\033[1;33m [*]PID:", pid, " ", event, " ", path, "\033[0m")
 		}
 
 		if err != nil {
 			logger.Error("\033[1;31m [-]error:", err, "\033[0m")
 		}
 	}
+}
+
+//RealFanotify 真正的fanotify处理函数 (动作，PID，路径，错误)
+func RealFanotify(notify *fanotify.NotifyFD) (string, int, string, error) {
+	data, err := notify.GetEvent(os.Getpid())
+	if err != nil {
+		return "", -1, "", fmt.Errorf("%w", err)
+	}
+
+	if data == nil {
+		return "", -1, "", err
+	}
+
+	defer data.Close()
+
+	path, err := data.GetPath()
+	if err != nil {
+		return "", -1, "", err
+	}
+
+	switch {
+	case data.MatchMask(unix.FAN_ACCESS):
+		return "FAN_ACCESS", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_OPEN):
+		return "Open", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_ATTRIB):
+		return "FAN_ATTRIB", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_CREATE):
+		return "FAN_CREATE", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_DELETE):
+		return "FAN_DELETE", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_MODIFY):
+		return "FAN_MODIFY", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_CLOSE):
+		return "FAN_CLOSE", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_MOVE):
+		return "FAN_MOVE", data.GetPID(), path, nil
+	case data.MatchMask(unix.FAN_CLOSE_WRITE):
+		return "FAN_CLOSE_WRITE", data.GetPID(), path, nil
+	}
+
+	return "", -1, "", fmt.Errorf("fanotify: unknown event")
+
 }
