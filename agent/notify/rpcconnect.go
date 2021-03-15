fe8267be6dabd9fe561ae766c81ef7d2f775ea3b
ZYA <zyazhb@yahoo.com>
2021-03-15 13:14:27 +0800

    Fix rpc _Client

--- agent/notify/rpcconnect.go
+++ agent/notify/rpcconnect.go
@@ -22,7 +22,7 @@ type ReportEvent struct {
 }
 
 var (
-	_Client    *rpc.Client
+	// _Client    *rpc.Client
 	_RpcMsg    chan *rpc.Call
 	_CanCancel chan bool
 )
@@ -34,51 +34,51 @@ func init() {
 
 //rpcreport inotify上报日志信息
 func rpcreporti(event fsnotify.Event, filehash string, serverip string) {
+	client, _ := rpc.DialHTTP("tcp", serverip)
 	var resp string
 	//上报内容
 	args := ReportEvent{AgentIP: geAgentIP(), FileName: event.Name, FileEvent: event.Op.String(), FileHash: filehash}
 
-	if nil == _Client {
+	if nil == client {
 		for {
-			if rpcReconnect(serverip) {
+			if rpcReconnect(serverip,client) {
 				break
 			}
 
 			time.Sleep(5000 * time.Millisecond)
 		}
 	}
-	_Client.Call(RPCReportEvent, args, &resp)
-	_Client = nil
+	client.Call(RPCReportEvent, args, &resp)
 }
 
 //rpcreport fanotify上报日志信息
 func rpcreportfan(FileName, FileEvent, filehash string, serverip string) {
+	client, _ := rpc.DialHTTP("tcp", serverip)
 	var resp string
 	//上报内容
 	args := ReportEvent{AgentIP: geAgentIP(), FileName: FileName, FileEvent: FileEvent, FileHash: filehash}
 
-	if nil == _Client {
+	if nil == client {
 		for {
-			if rpcReconnect(serverip) {
+			if rpcReconnect(serverip, client) {
 				break
 			}
 
 			time.Sleep(5000 * time.Millisecond)
 		}
 	}
-	_Client.Call(RPCReportEvent, args, &resp)
-	_Client = nil
+	client.Call(RPCReportEvent, args, &resp)
 }
 
 // rpcReconnect 重新连接rpc服务端
-func rpcReconnect(serverip string) bool {
+func rpcReconnect(serverip string, NewClient *rpc.Client) bool {
 	client, err := rpc.DialHTTP("tcp", serverip)
 	if err != nil {
 		log.Printf("ReDial RPC Server Error: %s", err)
 		return false
 	}
 
-	_Client = client
+	NewClient = client
 	log.Printf("ReDial RPC Server Sucess")
 
 	return true
